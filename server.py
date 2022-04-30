e024671ca6cf8cf777e5410deb315cc2e44ff61e
ViktoryaK <vkocherkevuch@gmail.com>
2022-04-30 18:15:06 +0300

    gg

--- server.py
+++ server.py
@@ -24,27 +24,30 @@ class Server:
             self.username_lookup[c] = username
             self.clients.append(c)
             print(self.clients)
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
 
     def broadcast(self, msg: str):
         for client in self.clients:
             client.send(msg.encode())
 
-    def handle_client(self, c: socket, addr): 
+    def handle_client(self, c: socket, addr):
         while True:
             username = c.recv(1024).decode()
-            for client in self.clients:
-                if client.username == username:
-                    N = client.keys['p'], client.keys['q']
-                    c.send(N).encode()
-                    c.send(client.keys['e']).encode()
-            messg = c.recv(1024).decode()
-            for client in self.clients:
-                if client.username == username:
-                    client.send(messg)
-            # for client in self.clients:
-            #     if client != c:
-            #         client.send(username)
+            try:
+                for client in self.clients:
+                    if client.username == username:
+                        N = client.keys['p'], client.keys['q']
+                        c.send(N).encode()
+                        c.send(client.keys['e']).encode()
+                messg = c.recv(1024).decode()
+                for client in self.clients:
+                    if client.username == username:
+                        client.send(messg)
+            except:
+                for client in self.clients:
+                    if client != c:
+                        client.send(username)
+
 
 if __name__ == "__main__":
     s = Server(9001)
